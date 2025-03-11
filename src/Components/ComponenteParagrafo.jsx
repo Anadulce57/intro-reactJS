1879fb23bb28f0143584584793da898025df7c5f
andl2008 <andl2008@hotmail.com>
2025-03-11 11:11:18 +0000

    Instalação do Tailwind e implementação dos recursos

--- src/Components/ComponenteParagrafo.jsx
+++ src/Components/ComponenteParagrafo.jsx
@@ -1,7 +1,7 @@
 export function ComponenteParafrafo(){
     return(
-        <div>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus quisquam mollitia nulla fugiat eius 
+        <div className="">
+            <p className="text-blue-800">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus quisquam mollitia nulla fugiat eius 
             amet esse quasi voluptatum fugit similique cum quae neque at nesciunt enim, animi dolorem dolor facilis!</p>
         </div>
     )
