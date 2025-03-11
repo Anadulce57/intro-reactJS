32f86c817f31e4a7b640c252795b4d46e9905f7c
andl2008 <andl2008@hotmail.com>
2025-03-11 10:48:47 +0000

    Colocando em prática a importação e exportação de componentes por named exports

---
+++ src/Components/ComponenteParagrafo.jsx
@@ -0,0 +1,8 @@
+export function ComponenteParafrafo(){
+    return(
+        <div>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Temporibus quisquam mollitia nulla fugiat eius 
+            amet esse quasi voluptatum fugit similique cum quae neque at nesciunt enim, animi dolorem dolor facilis!</p>
+        </div>
+    )
+}
